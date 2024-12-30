7c02d026e5dddd6bfbeb3bd87b3a2f692dd83a29
mohajjalinkhan <mohajjalin01@gmail.com>
2024-12-30 18:04:57 +0530

    Added todo input field and checkbox

--- lib/utils/todo_home_page.dart
+++ lib/utils/todo_home_page.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app_flutter/utils/todo_list.dart';
 
-class TodoHomePage extends StatelessWidget {
-    TodoHomePage({super.key});
-    // added array and set one sample value text = "" and for check box = false 
+class TodoHomePage extends StatefulWidget {
+  const TodoHomePage({super.key});
+
+  @override
+  State<TodoHomePage> createState() => _TodoHomePageState();
+}
+
+class _TodoHomePageState extends State<TodoHomePage> {
+  // added array and set one sample value text = "" and for check box = false
   List todoList = [
+    ["this is first todo in the app", false],
+    ["this is second todo in the app", false],
+  ];
 
-[ "this is first todo in the app", false],
-[ "this is second todo in the app", false],
-[ "this is third todo in the app", false],
-[ "this is second todo in the app", false],
-[ "this is third todo in the app", false],
-
- ];
+  // check box function
+  void chekBoxChanged(int index) {
+    setState(() {
+      todoList[index][1] = !todoList[index][1];
+    });
+  }
+  // adding add todo funcnality
 
   @override
   Widget build(BuildContext context) {
@@ -20,29 +29,56 @@ class TodoHomePage extends StatelessWidget {
       // added background color
       backgroundColor: Colors.blue.shade100,
       appBar: AppBar(
-        title: Text(
-          "Todo APP",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        title: Center(
+          child: Text(
+            "Todo APP",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
         ),
         backgroundColor: Colors.deepPurple,
       ),
       // using len of todo count
-       body: ListView.builder(itemCount: todoList.length,
-        itemBuilder: ( BuildContext context, index)
-      {
-        return TodoList(taskName: todoList[index][0]);
-       })
-      // Center(
-      //   child: Column(
-      //     mainAxisAlignment: MainAxisAlignment.center,
-      //     children: [
-      //       Text(
-      //         "Created first project ",
-      //         style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-      //       )
-      //     ],
-      //   ),
-      // ),
+      body: ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (BuildContext context, index) {
+          return TodoList(
+            taskName: todoList[index][0],
+            taskComplited: todoList[index][1],
+            onChange: (value) => chekBoxChanged(index),
+          );
+        },
+      ),
+      floatingActionButton: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Add todo items",
+                  filled: true,
+                  fillColor: Colors.purple.shade100,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.purple.shade900),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.purple.shade900),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          FloatingActionButton(
+            onPressed: addNewTask,
+            child: Icon(
+              Icons.add_box_rounded,
+              color: Colors.purple.shade900,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/utils/todo_list.dart
+++ lib/utils/todo_list.dart
@@ -1,33 +1,44 @@
 import 'package:flutter/material.dart';
 
-
 class TodoList extends StatelessWidget {
-  const TodoList({super.key, required this.taskName});
+  const TodoList(
+      {super.key,
+      required this.taskName,
+      required this.taskComplited,
+      this.onChange});
   final String taskName;
-  
+  final bool taskComplited;
+  final Function(bool?)? onChange;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-          padding: const EdgeInsets.only(top:20,left: 10,right: 10,bottom: 0),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.deepPurple,
-              borderRadius: BorderRadius.circular(15)
-            ),
-            padding: EdgeInsets.all(20),
-            child: Row(
-              children: [
-                Text(
-                  taskName,
-                style: TextStyle(
+      padding: const EdgeInsets.only(top: 20, left: 10, right: 10, bottom: 0),
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.purple.shade900,
+            borderRadius: BorderRadius.circular(15)),
+        padding: EdgeInsets.all(20),
+        child: Row(
+          children: [
+            Checkbox(
+                value: taskComplited,
+                onChanged: onChange,
+                side: BorderSide(color: Colors.white)),
+            Text(
+              taskName,
+              style: TextStyle(
                   fontSize: 18,
-                  color: Colors.white
-                ),
-                ),
-              ],
+                  color: Colors.white,
+                  decoration: taskComplited
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none
+                      , decorationColor: Colors.white),
+                      
             ),
-          ),
-        );
+          ],
+        ),
+      ),
+    );
   }
 }
